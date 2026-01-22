e4bf84a3224f3af15be60ed78aa6fb151d34d080
rworang <roy.worang.1992@gmail.com>
2026-01-22 15:44:27 +0100

    clear dist

--- dist/adapters/author.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @internal
- * Transforms raw WordPress author responses into clean Author objects.
- */
-import type { RawAuthor } from '../types/raw';
-import type { Author } from '../types/domain';
-/** @internal Converts a raw WordPress author to a clean Author object. */
-export declare const toAuthor: (raw: RawAuthor) => Author;
-//# sourceMappingURL=author.d.ts.map
