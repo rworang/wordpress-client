ce015d97101f8e1b0fe29cb9b72098c62d7dca89
rworang <roy.worang.1992@gmail.com>
2026-04-19 02:43:08 +0200

    feat(types): add PageWritePayload and TermWritePayload

--- src/types/payloads.ts
+++ src/types/payloads.ts
@@ -18,3 +18,30 @@ export interface PostWritePayload {
   date_gmt?: string
   password?: string
 }
+
+
+export interface PageWritePayload {
+  title?: string
+  content?: string
+  excerpt?: string
+  status?: 'publish' | 'draft' | 'pending' | 'private' | 'future'
+  slug?: string
+  parent?: number
+  menu_order?: number
+  author?: number
+  featured_media?: number
+  comment_status?: 'open' | 'closed'
+  ping_status?: 'open' | 'closed'
+  template?: string
+  meta?: Record<string, unknown>
+  date?: string
+  password?: string
+}
+
+export interface TermWritePayload {
+  name?: string
+  slug?: string
+  description?: string
+  parent?: number
+  meta?: Record<string, unknown>
+}
